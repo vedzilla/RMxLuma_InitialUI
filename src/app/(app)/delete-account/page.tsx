ef954b0d95ac9a8fcf1f1194badd419510bbe422
Jack Marshall <jack.er.marshall@gmail.com>
2026-02-27 16:01:49 +0000

    added delete account page for android

---
+++ src/app/(app)/delete-account/page.tsx
@@ -0,0 +1,9 @@
+export default function DeleteAccountPage() {
+  return (
+    <div className="py-12 px-[18px]">
+      <div className="max-w-3xl mx-auto">
+        <div className="bg-black rounded-2xl px-8 py-10 min-h-[400px]" />
+      </div>
+    </div>
+  );
+}
